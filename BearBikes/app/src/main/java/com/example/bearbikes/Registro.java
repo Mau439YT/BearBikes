dccab6791bd7183aa8dd2faa0746c42f07227aff
Mau <santiago.castillo.mauricio2@gmail.com>
2023-03-22 07:26:38 -0600

    Un buen de cambios

---
+++ BearBikes/app/src/main/java/com/example/bearbikes/Registro.java
@@ -0,0 +1,24 @@
+package com.example.bearbikes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+
+    public void registrarCiclista(View v){
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
+        finish();
+        Toast.makeText(this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
+    }
+}
