4af2a25c7a652965e1a7a4e7ef616c01c6d99bf0
Mau <santiago.castillo.mauricio2@gmail.com>
2023-03-22 07:13:25 -0600

    Un buen de cambios

--- BearBikes/app/src/main/java/com/example/bearbikes/LogIn.java
+++ BearBikes/app/src/main/java/com/example/bearbikes/LogIn.java
@@ -1,20 +1,26 @@
 package com.example.bearbikes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 
-    public void lanzarApp(View v){
+    public void iniciar(View v){
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void registrar(View v){
         Intent intent = new Intent(this, LogIn.class);
         startActivity(intent);
         finish();
