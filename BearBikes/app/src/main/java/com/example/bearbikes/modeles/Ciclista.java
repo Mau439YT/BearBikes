5369a5fea8751e7646e4ff987c717f3004744716
Mau <mauso.mty@gmail.com>
2023-03-23 09:49:16 -0600

    Se agrego parte de la conexion api

---
+++ BearBikes/app/src/main/java/com/example/bearbikes/modeles/Ciclista.java
@@ -0,0 +1,59 @@
+package com.example.bearbikes.modeles;
+
+public class Ciclista {
+
+    private String email;
+    private String password;
+    private String name;
+    private String apellidoPat;
+    private String apellidoMat;
+    private String celular;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellidoPat() {
+        return apellidoPat;
+    }
+
+    public void setApellidoPat(String apellidoPat) {
+        this.apellidoPat = apellidoPat;
+    }
+
+    public String getApellidoMat() {
+        return apellidoMat;
+    }
+
+    public void setApellidoMat(String apellidoMat) {
+        this.apellidoMat = apellidoMat;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+}
